8afa3051b53e1e532c9e647209b4a831f5201378
Manav Jain <118716368+ManavJain01@users.noreply.github.com>
2024-05-28 17:18:03 +0530

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 // Importing local files
 import Home from "./screens/Home"
 import Login from "./screens/Login"
+import Signup from "./screens/Signup"
 
 
 function App(){
@@ -12,6 +13,7 @@ function App(){
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/login" element={<Login />} />
+        <Route exact path="/createUser" element={<Signup />} />
       </Routes>
     </BrowserRouter>
   )
--- client/src/screens/Signup.jsx
+++ client/src/screens/Signup.jsx
@@ -1,7 +1,61 @@
+// Importing React Packages
+import { useState } from 'react';
+import { Link } from 'react-router-dom'
+
+
+
 export default function Signup(){
+  const [credentials, setCredentials] = useState({name:"", email:"", password:"", geolocation:""})
+
+  const handleSubmit = async (e) => {
+    //Synthetic event
+    e.preventDefault();
+    const response = await fetch("http://localhost:5000/api/createUser", {
+      method: 'POST',
+      headers:{
+        'Content-Type':'application/json'
+      },
+      body:JSON.stringify({name:credentials.name, email:credentials.email, password:credentials.password, location:credentials.geolocation})
+    });
+    
+    const json = await response.json()
+    console.log(json);
+
+    if(!json.success){
+      alert("Enter Valid Credentials")
+    }
+  }
+
+  const onChangeValue = (e) => {
+    setCredentials({...credentials, [e.target.name]:e.target.value})
+  }
+
   return (
-    <div>
-      
-    </div>
+    <form onSubmit={handleSubmit} className="bg-gray-950 text-white text-2xl h-lvh px-20 py-10 flex flex-col gap-10">
+      <div className="flex flex-col gap-2">
+        <label htmlFor="name">Enter your name</label>
+        <input type="text" name="name" value={credentials.name} onChange={onChangeValue} className="bg-transparent px-5 py-2 border-2 border-gray-700 rounded-lg outline-none" />
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <label htmlFor="email">Email address</label>
+        <input type="email" name="email" value={credentials.email} onChange={onChangeValue} className="bg-transparent px-5 py-2 border-2 border-gray-700 rounded-lg outline-none" />
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <label htmlFor="password">Email password</label>
+        <input type="text" name="password" value={credentials.password} onChange={onChangeValue} className="bg-transparent px-5 py-2 border-2 border-gray-700 rounded-lg outline-none" />
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <label htmlFor="geolocation">Enter Your Address</label>
+        <input type="text" name="geolocation" value={credentials.geolocation} onChange={onChangeValue} className="bg-transparent px-5 py-2 border-2 border-gray-700 rounded-lg outline-none" />
+      </div>
+
+      <div className="flex gap-5">
+        <button type="submit" className="bg-blue-400 w-fit px-5 py-1 rounded-lg">Submit</button>
+        <Link to="/login" className="bg-red-400 w-fit px-5 py-1 rounded-lg">Already a User</Link>
+      </div>
+    </form>
   )
 }
