e6a11b0e241370abf6104a7330c345e4b0e839d6
Manav Jain <118716368+ManavJain01@users.noreply.github.com>
2024-05-29 23:57:01 +0530

    commit

--- server/Routes/DisplayData.js
+++ server/Routes/DisplayData.js
@@ -2,9 +2,6 @@
 const express = require('express')
 const router = express.Router()
 
-// Importing local files
-const User = require('../models/User')
-
 router.post("/foodData",(req, res) => {
   try {
     res.send([global.food_items, global.foodCategory])
---
+++ server/Routes/OrderData.js
@@ -0,0 +1,5 @@
+// Importing Packages
+const express = require('express')
+const router = express.Router()
+
+// Importing local files
