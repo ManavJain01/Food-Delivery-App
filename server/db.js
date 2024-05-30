fa73fac5a157998d5c5b2d0bf5a3f67c0b9e1482
Manav Jain <118716368+ManavJain01@users.noreply.github.com>
2024-05-31 00:14:35 +0530

    commit

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@
 require("dotenv").config();
 
 const mongoose = require('mongoose')
-const mongoURI = process.env.MONGODB_URL
+const mongoURI = process.env.MONGODB_URI
 const mongoDB = async() => {
   try {
     await mongoose.connect(mongoURI);
